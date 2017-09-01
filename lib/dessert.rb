318afa95f0e0a6968d4d2e7d85bc8cf03ffa833f
Derek Devera <ddevera@g.clemson.edu>
2017-09-01 03:35:04 +0000

    Started Dessert

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,24 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
-  def initialize(flavor)
-    # your code here
+  attr_accessor :flavor
+  def initialize flavor
+    @name = "#{flavor} jelly bean"
+    @calories = 5
+  end
+  def delicious?
+    get_flavor == "licorice"
   end
 end
